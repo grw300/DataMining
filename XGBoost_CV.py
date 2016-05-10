27754691dccce65ad4e548533686bf88b9cebca7
grw300 <grw300@gmail.com>
2016-05-10 15:24:59 -0500

    Added new data columns based on existing data, mixed the two XGBoost models (one OneHotEncoded, the other raw) with a RandomForestRegressor. Kaggle Score: 0.90789

--- XGBoost_CV.py
+++ XGBoost_CV.py
@@ -6,8 +6,13 @@ import xgboost as xgb
 
 from sklearn.preprocessing import OneHotEncoder
 from sklearn.linear_model import LogisticRegression
+from sklearn.ensemble import RandomForestRegressor
 from sklearn import cross_validation, metrics
 from sklearn.grid_search import GridSearchCV
+
+
+def return_percentage(grp_index, group_index):
+    return float(len(grp_index)) / float(len(group_index))
 
 if __name__ == "__main__":
 
@@ -18,6 +23,41 @@ if __name__ == "__main__":
     X_train = kagTrainDat.ix[:, kagTrainDat.columns != 'ACTION']
 
     X_test = kagTestDat.ix[:, kagTestDat.columns != 'id']
+
+    X_all = pd.concat([X_test, X_train], ignore_index=True)
+
+    # Count/freq
+    print
+    "Counts"
+    for col in X_all.columns:
+        X_all['cnt' + col] = 0
+        groups = X_all.groupby([col])
+        for name, group in groups:
+            count = group[col].count()
+            X_all['cnt' + col].ix[group.index] = count
+
+    # Percent of dept that is this resource
+    for col in X_all.columns[1:6]:
+        X_all['Duse' + col] = 0.0
+        groups = X_all.groupby([col])
+        for name, group in groups:
+            grps = group.groupby(['RESOURCE'])
+            for rsrc, grp in grps:
+                X_all['Duse' + col].ix[grp.index] = float(len(grp.index)) / float(len(group.index))
+
+    # Number of resources that a manager manages
+    for col in X_all.columns[0:1]:
+        if col == 'MGR_ID':
+            continue
+        print
+        col
+        X_all['Mdeps' + col] = 0
+        groups = X_all.groupby(['MGR_ID'])
+        for name, group in groups:
+            X_all['Mdeps' + col].ix[group.index] = len(group[col].unique())
+
+    X_train = X_all[:][X_all.index >= len(X_test.index)]
+    X_test = X_all[:][X_all.index < len(X_test.index)]
 
     D_train = xgb.DMatrix(X_train.values, label=y_train.values)
     D_test = xgb.DMatrix(X_test.values)
@@ -51,7 +91,13 @@ if __name__ == "__main__":
 
     y_test_enc = gbf_lm.predict_proba(gbf_enc.transform(X_test))[:, 1]
 
-    y_sub = 0.45*y_test + 0.55*y_test_enc
+    model = RandomForestRegressor(n_estimators=6000, oob_score=True, n_jobs=-1, random_state=42, \
+                                  max_features=.9, min_samples_leaf=3)
+    model.fit(X_train, y_train)
+
+    y_test_rnd_tree = model.predict(X_test)
+
+    y_sub = (y_test + y_test_enc + y_test_rnd_tree)/3.0
 
     submission = pd.Series(data=y_sub, name='Action', index=kagTestDat['id'])
 
